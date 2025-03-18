706dd9636ada7b6faab4dc72626e7f31fb3fc0ca
Jesutofunmi <tofunmi73@gmail.com>
2025-03-18 14:36:14 +0100

    add allowed host

--- spotter_trip_app/settings.py
+++ spotter_trip_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-m$q0whgt8m^=*s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost 127.0.0.1').split(' ')
+ALLOWED_HOSTS = ["tripbackend-4e6p.onrender.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
